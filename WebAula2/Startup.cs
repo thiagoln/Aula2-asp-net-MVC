efce66b7467de9ac79840c539b019bc32b690aad
Thiago Lemes <thigo_bl@hotmail.com>
2017-08-23 21:32:34 -0400

    enviando primeira versao do sistema

---
+++ WebAula2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAula2.Startup))]
+namespace WebAula2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
